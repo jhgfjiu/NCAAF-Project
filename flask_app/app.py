32401cfd70fc53487ee974c7a8882ae7d815a835
vroom0607 <mattyandou@gmail.com>
2025-09-17 10:24:13 -0700

    update

--- flask_app/app.py
+++ flask_app/app.py
@@ -13,12 +13,6 @@ POSTGRES_DB = os.getenv("POSTGRES_DB")
 POSTGRES_HOST = os.getenv("POSTGRES_HOST")
 POSTGRES_PORT = os.getenv("POSTGRES_PORT")
 
-print("POSTGRES_USER:", os.getenv("POSTGRES_USER"))
-print("POSTGRES_PASSWORD:", os.getenv("POSTGRES_PASSWORD"))
-print("POSTGRES_DB:", os.getenv("POSTGRES_DB"))
-print("POSTGRES_HOST:", os.getenv("POSTGRES_HOST"))
-print("POSTGRES_PORT:", os.getenv("POSTGRES_PORT"))
-
 app.config['SQLALCHEMY_DATABASE_URI'] = (
     f"postgresql://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}"
     f"@{os.getenv('POSTGRES_HOST')}:{os.getenv('POSTGRES_PORT')}/"
